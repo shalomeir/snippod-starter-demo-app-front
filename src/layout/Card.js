cc3d2e632f9e07e38e708e9b6afd8dbb8610f93f
Seonggyu Lee <shalomeir@gmail.com>
2016-07-18 21:53:56 +0900

    add Cards and props, state change

--- src/layout/Card.js
+++ src/layout/Card.js
@@ -1,15 +1,41 @@
 import React, { Component, PropTypes } from 'react';
+import classNames from 'classnames';
+
+import Button from './Button';
 
 export default class Card extends Component {
+
+  static propTypes = {
+    title: PropTypes.string.isRequired
+  };
+
+  static defaultProps = {
+    title: '디폴트 제목입니다.'
+  };
+
+  constructor() {
+    super();
+    this.changeColor = this.changeColor.bind(this);
+  }
+
+  state = {
+    color: 'blue'
+  };
+
+  changeColor() {
+    console.log('click button');
+    this.setState({ color: 'green' });
+  }
 
   render() {
 
     return (
-      <div className="ui inverted blue padded text segment center aligned">
-        <h2>웰컴!</h2>
+      <div className={classNames('ui inverted padded text segment center aligned', this.state.color)}>
+        <h2>{this.props.title}</h2>
         <p>
           만나서 반갑습니다.
         </p>
+        <Button onClick={this.changeColor}/>
       </div>
     );
   }
