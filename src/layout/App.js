ba341b6d01347c2e0d3bb467085c235f194a0f46
Seonggyu Lee <shalomeir@gmail.com>
2016-07-18 23:08:17 +0900

    update Card and App for component LifeCycle

--- src/layout/App.js
+++ src/layout/App.js
@@ -7,12 +7,31 @@ export default class App extends Component {
     children: PropTypes.object.isRequired
   };
 
+  constructor() {
+    super();
+    this.changeColor = this.changeColor.bind(this);
+  }
+
+  state = {
+    color: 'blue'
+  };
+
+  changeColor() {
+    console.log('click button');
+    if (this.state.color !== 'green') {
+      this.setState({ color: 'green' });
+    } else {
+      this.setState({ color: 'blue' });
+    }
+  }
+
   render() {
 
     return (
       <div id="app" className="app" >
-        <Card title="Welcome"/>
-        <Card title="이건 또 다른 카드"/>
+        <Card title="Welcome" color={this.state.color} changeColor={this.changeColor}/>
+        <Card color={this.state.color} changeColor={this.changeColor} />
+        <Card color={this.state.color} color2={'black'} changeColor={this.changeColor} />
       </div>
     );
   }
