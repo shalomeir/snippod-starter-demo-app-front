7df62d0f17047e281dc5543aec254bff82bfd865
Seonggyu Lee <shalomeir@gmail.com>
2016-07-23 00:54:28 +0900

    add application history utils and inject redux connection to Home.js

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,14 +1,38 @@
 import React, { Component, PropTypes } from 'react';
 import Radium from 'radium';
 import Helmet from 'react-helmet';
+import { connect } from 'react-redux';
 
+import { pushPath } from 'ducks/application/application';
+
+@connect(
+  null,
+  { pushPath }
+)
+@Radium
 export default class Home extends Component {
+  static propTypes = {
+    location: PropTypes.object.isRequired,
+    pushPath: PropTypes.func.isRequired
+  };
+
+  constructor() {
+    super();
+    this.moveToUser = this.moveToUser.bind(this);
+  }
+
+  moveToUser() {
+    this.props.pushPath('/user/' + this.refs.userId.value);
+  }
 
   render() {
     return (
       <div className="home ui text container main-container">
         <Helmet title="Home"/>
-        This is a Home.
+        <div className="ui input">
+          <input type="text" name="user" placeholder={'type user id'} ref="userId"/>
+          <button type="button" className="ui grey button" onClick={this.moveToUser} > Go! </button>
+        </div>
       </div>
     );
   }
---
+++ src/ducks/application/application.js
@@ -0,0 +1,150 @@
+const debug = require('utils/getDebugger')('application');
+import { browserHistory as history } from 'react-router';
+
+// This function is used for override query url
+export function overrideQuery(query) {
+  return (dispatch, getState) => {
+    const location = getState().routing.location;
+    history.replace({
+      state: location.state ? location.state : null,
+      pathname: location.pathname,
+      query: Object.assign(location.query, query)
+    });
+  };
+}
+
+export function pushQuery(query) {
+  return (dispatch, getState) => {
+    const location = getState().routing.location;
+    history.push({
+      state: location.state ? location.state : null,
+      pathname: location.pathname,
+      query
+    });
+  };
+}
+
+export function deleteQuery(queryKey) {
+  return (dispatch, getState) => {
+    const location = getState().routing.location;
+    if (location.query[queryKey]) {
+      delete location.query[queryKey];
+      history.push(location);
+    }
+  };
+}
+
+export function pushHistoryState(state) {
+  return (dispatch, getState) => {
+    const location = getState().routing.location;
+    history.push({
+      state,
+      pathname: location.pathname,
+      query: location.query
+    });
+  };
+}
+
+export function replaceHistoryState(state) {
+  return (dispatch, getState) => {
+    const location = getState().routing.location;
+    history.replace({
+      state,
+      pathname: location.pathname,
+      query: location.query
+    });
+  };
+}
+
+export function overrideNextPathname(nextPathname) {
+  return (dispatch, getState) => {
+    const location = getState().routing.location;
+    const state = location.state ? location.state : {};
+    history.replace({
+      state: Object.assign(state, { nextPathname }),
+      pathname: location.pathname,
+      query: Object.assign(location.query, { redirect: encodeURIComponent(nextPathname) })
+    });
+  };
+}
+
+export function pushPath(pathname) {
+  return () => {
+    history.push(pathname);
+  };
+}
+
+export function replacePath(pathname) {
+  return () => {
+    history.replace(pathname);
+  };
+}
+
+export function pushPathWithQuery(pathname) {
+  return (dispatch, getState) => {
+    const location = getState().routing.location;
+    history.push({
+      pathname,
+      query: location.query
+    });
+  };
+}
+
+export function pushLocation(location) {
+  return () => {
+    history.push(location);
+  };
+}
+
+export function replaceLocation(location) {
+  return () => {
+    history.replace(location);
+  };
+}
+
+
+// https://github.com/reactjs/react-router/issues/1982
+export function refreshPage() {
+  return (dispatch, getState) => {
+    const location = getState().routing.location;
+    history.replace('/');
+    history.replace(location.pathname);
+  };
+}
+
+//If defaultPathname and redirect query and nextPathname is not exist, do nothing.
+export function redirectPushPath(defaultPathname) {
+
+  return (dispatch, getState) => {
+    const location = getState().routing.location;
+    let pathname = defaultPathname;
+
+    if (location.query.redirect) {
+      pathname = decodeURIComponent(location.query.redirect);
+    } else if (location.state && location.state.nextPathname) {
+      pathname = location.state.nextPathname;
+    }
+
+    if (pathname) {
+      history.push(pathname);
+    }
+  };
+}
+
+export function redirectReplacePath(defaultPathname) {
+
+  return (dispatch, getState) => {
+    const location = getState().routing.location;
+    let pathname = defaultPathname;
+
+    if (location.query.redirect) {
+      pathname = decodeURIComponent(location.query.redirect);
+    } else if (location.state && location.state.nextPathname) {
+      pathname = location.state.nextPathname;
+    }
+
+    if (pathname) {
+      history.replace(pathname);
+    }
+  };
+}
