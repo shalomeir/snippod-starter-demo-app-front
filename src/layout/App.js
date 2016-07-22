fb3017ed0575a4323e0f944859f26b24f1c40c02
Seonggyu Lee <shalomeir@gmail.com>
2016-07-22 23:52:57 +0900

    update Card and App for component LifeCycle

--- src/layout/App.js
+++ src/layout/App.js
@@ -3,13 +3,35 @@ import { render } from 'react-dom';
 import Card from './Card';
 
 export default class App extends Component {
+  static propTypes = {
+    children: PropTypes.object.isRequired
+  };
+
+  constructor() {
+    super();
+    this.changeColor = this.changeColor.bind(this);
+  }
+
+  state = {
+    color: 'blue'
+  };
+
+  changeColor() {
+    console.log('click button');
+    if (this.state.color !== 'green') {
+      this.setState({ color: 'green' });
+    } else {
+      this.setState({ color: 'blue' });
+    }
+  }
 
   render() {
 
     return (
       <div id="app" className="app" >
-        <Card title="Welcome"/>
-        <Card title="이건 또 다른 카드"/>
+        <Card title="Welcome" color={this.state.color} changeColor={this.changeColor}/>
+        <Card color={this.state.color} changeColor={this.changeColor} />
+        <Card color={this.state.color} color2={'black'} changeColor={this.changeColor} />
       </div>
     );
   }
--- src/layout/Card.js
+++ src/layout/Card.js
@@ -6,36 +6,47 @@ import Button from './Button';
 export default class Card extends Component {
 
   static propTypes = {
-    title: PropTypes.string.isRequired
+    title: PropTypes.string.isRequired,
+    color: PropTypes.string.isRequired,
+    color2: PropTypes.string,
+    changeColor: PropTypes.func.isRequired
   };
 
   static defaultProps = {
     title: '디폴트 제목입니다.'
   };
 
-  constructor() {
-    super();
-    this.changeColor = this.changeColor.bind(this);
-  }
-
   state = {
-    color: 'blue'
+    color: null
   };
 
-  changeColor() {
-    console.log('click button');
-    this.setState({ color: 'green' });
+  componentWillMount() {
+    if (this.props.color === 'blue' && this.props.color2) {
+      this.setState({ color: 'pink' });
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.color2) {
+      if (nextProps.color === 'green') {
+        this.setState({ color: this.props.color2 });
+      } else {
+        this.setState({ color: nextProps.color });
+      }
+    }
   }
 
   render() {
 
+    const color = this.state.color ? this.state.color : this.props.color;
+
     return (
-      <div className={classNames('ui inverted padded text segment center aligned', this.state.color)}>
+      <div className={classNames('ui inverted padded text segment center aligned', color)}>
         <h2>{this.props.title}</h2>
         <p>
           만나서 반갑습니다.
         </p>
-        <Button onClick={this.changeColor}/>
+        <Button onClick={this.props.changeColor}/>
       </div>
     );
   }
